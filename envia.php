edc614f3b788095521fdc5214af2e4d725e77788
Xh30Music <xh30music@gmail.com>
2024-10-17 13:50:54 -0400

    Add files via upload

---
+++ envia.php
@@ -0,0 +1,26 @@
+﻿<?php	
+$remitente = $_POST['email'];
+$destinatario = 'xh30music@gmail.com';
+$asunto = "Message";
+if (!$_POST){
+?>
+
+<?php
+}else{
+	 
+    $cuerpo = "Nombre y apellido: " . $_POST["name"] . "\r\n"; 
+    $cuerpo .= "Email: " . $_POST["email"] . "\r\n";
+	$cuerpo .= "Consulta: " . $_POST["message"] . "\r\n";
+
+    $headers  = "MIME-Version: 1.0\n";
+    $headers .= "Content-type: text/plain; charset=utf-8\n";
+    $headers .= "X-Priority: 3\n";
+    $headers .= "X-MSMail-Priority: Normal\n";
+    $headers .= "X-Mailer: php\n";
+    $headers .= "From: \"".$_POST["name"]." ".$_POST["apellido"]."\" <".$remitente.">\n";
+
+    mail($destinatario, $asunto, $cuerpo, $headers);
+    
+    include "confirma.html";
+}
+?>
